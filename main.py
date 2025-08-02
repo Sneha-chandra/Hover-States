0be27a4aa757d47feac6e05e21eb25b6dd303bbe
Hardik Bagga <hardikbagga121@gmail.com>
2025-08-02 11:13:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -162,4 +162,5 @@ def health():
         return {"status": "unhealthy", "error": str(e)}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host= '0.0.0.0', debug=True, port=5000)
+ 
